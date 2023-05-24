e3e54c6fe3ef724d444d22f9f9984dbccad4e8ea
Holder-hash <X-Lamamen-X@yandex.ru>
2023-05-24 19:38:49 +0300

    theme dark mode path (text color)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 function darkMode() {
     const body = document.body;
     const wrapper = document.querySelector('.wrapper');
-    const subBlockHeader = document.querySelector('.sub-block-header');
-    const subBlockDescription = document.querySelector('.sub-block-description');
+    const subBlockHeader = document.querySelectorAll('.sub-block-header');
+    const subBlockDescription = document.querySelectorAll('.sub-block-description');
     const wasDarkMode = localStorage.getItem('darkMode') === 'true';
+
+    subBlockHeader.forEach(header => {
+        header.classList.toggle('dark-mode-text', !wasDarkMode)   
+    });
+
+    subBlockDescription.forEach(description => {
+        description.classList.toggle('dark-mode-text', !wasDarkMode)   
+    });
 
 
     localStorage.setItem('darkMode', !wasDarkMode)
     body.classList.toggle('dark-mode', !wasDarkMode)
     wrapper.classList.toggle('dark-mode', !wasDarkMode)
-    subBlockHeader.classList.toggle('dark-mode-text', !wasDarkMode)
-    subBlockDescription.classList.toggle('dark-mode-text', !wasDarkMode)
 
     if (!wasDarkMode) {
         document.querySelector('.theme-btn').textContent = 'wb_sunny'
