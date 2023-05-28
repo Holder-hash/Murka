b1d45de2adff733c3cfdc0cbefe7be4a3fd73d19
Vladosik333222111 <vladislav.baschkin@gmail.com>
2023-05-28 22:35:47 +0300

    add support list and media support

--- js/support-list.js
+++ js/support-list.js
@@ -4,6 +4,12 @@ const line3 = document.getElementById('info-line-3');
 const line4 = document.getElementById('info-line-4');
 const line5 = document.getElementById('info-line-5');
 const line6 = document.getElementById('info-line-6');
+const ltxt1 = document.getElementById('text_support_convolutions_1');
+const ltxt2 = document.getElementById('text_support_convolutions_2');
+const ltxt3 = document.getElementById('text_support_convolutions_3');
+const ltxt4 = document.getElementById('text_support_convolutions_4');
+const ltxt5 = document.getElementById('text_support_convolutions_5');
+const ltxt6 = document.getElementById('text_support_convolutions_6');
 let order1 = 0;
 let order2 = 0;
 let order3 = 0;
@@ -13,7 +19,7 @@ let order6 = 0;
 
 line1.addEventListener('click', () =>{
     if(order1 === 0){
-        line1.style.height = '240px';
+        line1.style.height = '340px';
         order1 = 1;
         line2.style.height = '60px';
         line3.style.height = '60px';
@@ -25,8 +31,15 @@ line1.addEventListener('click', () =>{
         order4 = 0;
         order5 = 0;
         order6 = 0;
+        ltxt1.style.display = 'block';
+        ltxt2.style.display = 'none';
+        ltxt3.style.display = 'none';
+        ltxt4.style.display = 'none';
+        ltxt5.style.display = 'none';
+        ltxt6.style.display = 'none';
     } 
     else {
+        ltxt1.style.display = 'none';
         line1.style.height = '60px';
         order1 = 0;
         
@@ -34,13 +47,19 @@ line1.addEventListener('click', () =>{
 });
 line2.addEventListener('click', () =>{
     if(order2 === 0){
-        line2.style.height = '240px';
+        line2.style.height = '340px';
         order2 = 1;
         line1.style.height = '60px';
         line3.style.height = '60px';
         line4.style.height = '60px';
         line5.style.height = '60px';
         line6.style.height = '60px';
+        ltxt1.style.display = 'none';
+        ltxt2.style.display = 'block';
+        ltxt3.style.display = 'none';
+        ltxt4.style.display = 'none';
+        ltxt5.style.display = 'none';
+        ltxt6.style.display = 'none';
         order1 = 0;
         order3 = 0;
         order4 = 0;
@@ -48,19 +67,26 @@ line2.addEventListener('click', () =>{
         order6 = 0;
     } 
     else {
+        ltxt2.style.display = 'none';
         line2.style.height = '60px';
         order2 = 0;
     }
 });
 line3.addEventListener('click', () =>{
     if(order3 === 0){
-        line3.style.height = '240px';
+        line3.style.height = '340px';
         order3 = 1;
         line2.style.height = '60px';
         line1.style.height = '60px';
         line4.style.height = '60px';
         line5.style.height = '60px';
         line6.style.height = '60px';
+        ltxt1.style.display = 'none';
+        ltxt2.style.display = 'none';
+        ltxt3.style.display = 'block';
+        ltxt4.style.display = 'none';
+        ltxt5.style.display = 'none';
+        ltxt6.style.display = 'none';
         order2 = 0;
         order1 = 0;
         order4 = 0;
@@ -68,19 +94,26 @@ line3.addEventListener('click', () =>{
         order6 = 0;
     } 
     else {
+        ltxt3.style.display = 'none';
         line3.style.height = '60px';
         order3 = 0;
     }
 });
 line4.addEventListener('click', () =>{
     if(order4 === 0){
-        line4.style.height = '240px';
+        line4.style.height = '340px';
         order4 = 1;
         line2.style.height = '60px';
         line3.style.height = '60px';
         line1.style.height = '60px';
         line5.style.height = '60px';
         line6.style.height = '60px';
+        ltxt1.style.display = 'none';
+        ltxt2.style.display = 'none';
+        ltxt3.style.display = 'none';
+        ltxt4.style.display = 'block';
+        ltxt5.style.display = 'none';
+        ltxt6.style.display = 'none';
         order2 = 0;
         order3 = 0;
         order1 = 0;
@@ -88,19 +121,26 @@ line4.addEventListener('click', () =>{
         order6 = 0;
     } 
     else {
+        ltxt4.style.display = 'none';
         line4.style.height = '60px';
         order4 = 0;
     }
 });
 line5.addEventListener('click', () =>{
     if(order5 === 0){
-        line5.style.height = '240px';
+        line5.style.height = '340px';
         order5 = 1;
         line2.style.height = '60px';
         line3.style.height = '60px';
         line4.style.height = '60px';
         line1.style.height = '60px';
         line6.style.height = '60px';
+        ltxt1.style.display = 'none';
+        ltxt2.style.display = 'none';
+        ltxt3.style.display = 'none';
+        ltxt4.style.display = 'none';
+        ltxt5.style.display = 'block';
+        ltxt6.style.display = 'none';
         order2 = 0;
         order3 = 0;
         order4 = 0;
@@ -108,19 +148,26 @@ line5.addEventListener('click', () =>{
         order6 = 0;
     } 
     else {
+        ltxt5.style.display = 'none';
         line5.style.height = '60px';
         order5 = 0;
     }
 });
 line6.addEventListener('click', () =>{
     if(order6 === 0){
-        line6.style.height = '240px';
+        line6.style.height = '340px';
         order6 = 1;
         line2.style.height = '60px';
         line3.style.height = '60px';
         line4.style.height = '60px';
         line5.style.height = '60px';
         line1.style.height = '60px';
+        ltxt1.style.display = 'none';
+        ltxt2.style.display = 'none';
+        ltxt3.style.display = 'none';
+        ltxt4.style.display = 'none';
+        ltxt5.style.display = 'none';
+        ltxt6.style.display = 'block';
         order2 = 0;
         order3 = 0;
         order4 = 0;
@@ -128,6 +175,7 @@ line6.addEventListener('click', () =>{
         order1 = 0;
     } 
     else {
+        ltxt6.style.display = 'none';
         line6.style.height = '60px';
         order6 = 0;
     }
