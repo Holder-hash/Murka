f5184c3acdef91a17505707c735f0ab57cc78559
Holder-hash <X-Lamamen-X@yandex.ru>
2023-05-28 11:12:59 +0300

    little footer added, footer pos absolute

--- js/page-change.js
+++ js/page-change.js
@@ -1,6 +1,12 @@
 const header = document.querySelector('header')
+const footer = document.querySelector('footer')
 
 document.addEventListener('DOMContentLoaded', function() {
     header.classList.toggle('little-header')
     document.querySelector('.header-main-box').style = 'display: none'
+
+    footer.classList.toggle('little-footer')
+    document.querySelector('.countenier_btn').style = 'display: none'
+    document.querySelector('.countenier_footer').style = 'height: 200px'
+
 })
