0ead845a60dccba2b8e1b190d1ab10cfe197490f
Holder-hash <X-Lamamen-X@yandex.ru>
2023-05-29 22:43:09 +0300

    better hyunya

--- js/commands-page.js
+++ js/commands-page.js
@@ -1,19 +1,47 @@
-const expansionPanelHeader = document.querySelectorAll('.expansion-panel-header');
-const expansionPanelBody = document.querySelector('.expansion-panel-body');
+var containers;
+function initDrawers() {
+	// Get the containing elements
+	containers = document.querySelectorAll(".container");
+	setHeights();
+	wireUpTriggers();
+	window.addEventListener("resize", setHeights);
+}
 
-var expansed = false;
+window.addEventListener("load", initDrawers);
 
-expansionPanelHeader.forEach(expansionElement => {
-    expansionElement.addEventListener('click', () => {
-            if (expansed == false) {
-                expansed = true;
-                expansionPanelBody.style = `height: 350px`;
-            }       
-            else if (expansed == true){
-                expansed = false;
-                expansionPanelBody.style = `height: 0px`;
-            }
-        })
-    
-    }
-);
+function setHeights() {
+	containers.forEach(container => {
+		// Get content
+		let content = container.querySelector(".content");
+		// Needed if this is being fired after a resize
+		content.removeAttribute("aria-hidden");
+		// Height of content to show/hide
+		let heightOfContent = content.getBoundingClientRect().height;
+		// Set a CSS custom property with the height of content
+		container.style.setProperty("--containerHeight", `${heightOfContent}px`);
+		// Once height is read and set
+		setTimeout(e => {
+			container.classList.add("height-is-set");
+			content.setAttribute("aria-hidden", "true");
+		}, 0);
+	});
+}
+
+function wireUpTriggers() {
+	containers.forEach(container => {
+		// Get each trigger element
+		let btn = container.querySelector(".trigger");
+		// Get content
+		let content = container.querySelector(".content");
+		btn.addEventListener("click", () => {
+			container.setAttribute(
+				"data-drawer-showing",
+				container.getAttribute("data-drawer-showing") === "true" ? "false" : "true"
+			);
+			content.setAttribute(
+				"aria-hidden",
+				content.getAttribute("aria-hidden") === "true" ? "false" : "true"
+			);
+		});
+	});
+}
