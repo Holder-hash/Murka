e06d580f274ec1caa12ce08bda3f9f4fd71460a3
Holder-hash <X-Lamamen-X@yandex.ru>
2023-05-31 19:13:57 +0300

    header localisation

--- js/language.js
+++ js/language.js
@@ -25,12 +25,32 @@ function translateENG() {
     localStorage.setItem('english', 'true');
 
     document.getElementById('pageDocumentation').textContent = 'documentation'
+    document.getElementById('pageCommands').textContent = 'commands'
+    document.getElementById('pageStatistic').textContent = 'statistics'
+    document.getElementById('pageSupportUs').textContent = 'support us'
+
+    document.querySelector('.hint_box_text').textContent = 'community'
+    document.querySelector('.tool-bar___login-box p').textContent = 'sign in'
+    document.querySelector('.header-main-box___text-box p').textContent = 'Upload music to your server'
+    document.querySelector('.header-main-box___add-ds-button span').textContent = 'ADD TO DISCORD'
+    document.querySelector('.header-main-box___more-button span').textContent = 'MORE'
+    
 }
 
 function translateRUS() {
     localStorage.setItem('english', 'false')
 
     document.getElementById('pageDocumentation').textContent = 'документация'
+    document.getElementById('pageCommands').textContent = 'команды'
+    document.getElementById('pageStatistic').textContent = 'статистика'
+    document.getElementById('pageSupportUs').textContent = 'поддержите нас'
+
+    document.querySelector('.hint_box_text').textContent = 'сообщество'
+    document.querySelector('.tool-bar___login-box p').textContent = 'Войти'
+    document.querySelector('.header-main-box___text-box p').textContent = 'Залей музыки в свой сервер'
+    document.querySelector('.header-main-box___add-ds-button span').textContent = 'ДОБАВИТЬ В DISCOR'
+    document.querySelector('.header-main-box___more-button span').textContent = 'ПОДРОБНЕЕ'
+
 }
 
 languageBtnENG.addEventListener('click', translateENG);
