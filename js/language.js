89b6b154f0cde9dae4fac3f6e6d84e343a75211e
Holder-hash <X-Lamamen-X@yandex.ru>
2023-05-31 20:54:13 +0300

    main page localisation

--- js/language.js
+++ js/language.js
@@ -49,6 +49,33 @@ function translateENG() {
     document.querySelector('.footer-menu7').textContent = 'Cookies'
     document.querySelector('.footer-menu8').textContent = 'State'
     document.querySelector('.footer-menu9').textContent = 'Donations'
+
+    //main content
+    document.querySelector('.sbh1').textContent = 'Flexible functionality'
+    document.querySelector('.sbd1').textContent = 'Feature rich, well structured and intuitive control panel with many settings! Set up everything in one place: prefix, language, command permissions and much more!'
+
+    document.querySelector('.sbh2').textContent = 'Custom commands'
+    document.querySelector('.sbd2').textContent = 'Create your own commands that send messages to channels, add or remove roles, add reactions, or trigger other button and menu actions.'
+    document.querySelector('.sbd22').textContent = 'You can go further and create more complex commands with programmable behavior using message templates! Check out our documentation for more details.'
+
+    document.querySelector('.sbh3').textContent = 'Moderation'
+    document.querySelector('.sbd3').textContent = 'Keep your Discord server secure with advanced moderation features such as temporary bans, mutes, and member warnings.'
+    document.querySelector('.sbd33').textContent = 'Murka also has a customizable automatic moderation system against unwanted links, invites, bad words, flooding, zalgo, excessive use of Caps Lock, emotions and mentions.'
+
+    document.querySelector('.sbh4').textContent = 'Social media'
+    document.querySelector('.sbd4').textContent = 'Follow your favorite YouTube channels, Twitch.tv or Trovo.live streamers, get notifications for new videos and live streams.'
+    document.querySelector('.sbd44').textContent = 'Set up automatic publication of posts from your VKontakte community directly to a text channel on the Discord server.'
+
+    document.querySelector('.sbh5').textContent = 'User Rating'
+    document.querySelector('.sbd5').textContent = 'Your community is priceless! Reward the most active members using our ranking system. Let them reward each other with cookies and track their voice activity.'
+    document.querySelector('.sbd55').textContent = 'Love your community <3'
+
+    document.querySelector('.sbh6').textContent = 'Audit system'
+    document.querySelector('.sbd6').textContent = 'If the built-in auditing in Discord is not enough, set up additional logging for messages deleted or modified, joining and leaving voice channels, changing member roles, and much more.'
+    document.querySelector('.sbd6').textContent = 'The audit system uses Webhooks to send logs. You can change their name, set your own avatar and combine several logs into one webhook.'
+
+    document.querySelector('.sbh7').textContent = 'Huge selection of music'
+    document.querySelector('.sbd7').textContent = 'Listen to your favorite music from Yandex.Music, SoundCloud, Bandcamp, Vimeo, Twitch.tv and radio stations. DJ roles, playlists, convenient button control.'
 }
 
 function translateRUS() {
@@ -79,6 +106,33 @@ function translateRUS() {
     document.querySelector('.footer-menu7').textContent = 'Файлы Cookie'
     document.querySelector('.footer-menu8').textContent = 'Состояние'
     document.querySelector('.footer-menu9').textContent = 'Пожертвования'
+
+    //main content
+    document.querySelector('.sbh1').textContent = 'Гибкий функционал'
+    document.querySelector('.sbd1').textContent = 'Многофункциональная, хорошо структурированная и интуитивно понятная панель управления с множеством настроек! Настраивайте всё в одном месте: префикс, язык, права доступа к командам и многое другое!'
+
+    document.querySelector('.sbh2').textContent = 'Настраиваемые команды'
+    document.querySelector('.sbd2').textContent = 'Создавайте свои собственные команды, которые отправляют сообщения в каналы, добавляют или снимают роли, добавляют реакции или вызывают другие действия по кнопкам и меню.'
+    document.querySelector('.sbd22').textContent = 'Вы можете пойти дальше и создавать более сложные команды с программируемым поведением используя шаблоны сообщений! Ознакомьтесь с нашей документацией для получения более подробной информации.'
+
+    document.querySelector('.sbh3').textContent = 'Модерирование'
+    document.querySelector('.sbd3').textContent = 'Обеспечьте безопасность своего Discord-сервера с помощью расширенных функций модерации, таких как временные баны, мьюты и предупреждения участников.'
+    document.querySelector('.sbd33').textContent = 'Murka также имеет настраиваемую систему автоматической модерации против нежелательных ссылок, приглашений, плохих слов, флуда, zalgo, чрезмерного использования Caps Lock, эмоций и упоминаний.'
+
+    document.querySelector('.sbh4').textContent = 'Социальные сети'
+    document.querySelector('.sbd4').textContent = 'Следите за любимыми каналами YouTube, стримерами Twitch.tv или Trovo.live, получайте уведомления о новых видео и прямых трансляциях.'
+    document.querySelector('.sbd44').textContent = 'Настройте автоматическую публикацию постов из своего сообщества ВКонтакте сразу в текстовый канал на Discord сервере.'
+
+    document.querySelector('.sbh5').textContent = 'Рейтинг пользователей'
+    document.querySelector('.sbd5').textContent = 'Ваше сообщество бесценно! Награждайте самых активных участников используя нашу систему рейтинга. Позвольте им поощрять друг друга печеньками и отслеживать свою голосовую активность.'
+    document.querySelector('.sbd55').textContent = 'Любите своё сообщество <3'
+
+    document.querySelector('.sbh6').textContent = 'Система аудита'
+    document.querySelector('.sbd6').textContent = 'Если встроенного в Discord аудита недостаточно, настройте дополнительное логирование событий удаления или изменения сообщений, присоединения и выхода из голосовых каналов, изменения ролей участников и многое другое.'
+    document.querySelector('.sbd66').textContent = 'Система аудита использует Вебхуки для отправки логов. Вы можете менять их название, устанавливать свою аватарку и комбинировать несколько логов в один вебхук.    '
+
+    document.querySelector('.sbh7').textContent = 'Огромный выбор музыки'
+    document.querySelector('.sbd7').textContent = 'Слушайте Вашу любимую музыку из Яндекс.Музыки, SoundCloud, Bandcamp, Vimeo, Twitch.tv и радиостанции. DJ-роли, плейлисты, удобное управление кнопками.'
 }
 
 languageBtnENG.addEventListener('click', translateENG);
