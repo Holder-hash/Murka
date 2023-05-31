aaa3831a85176d77bdb084a102a2389455a163c2
Holder-hash <X-Lamamen-X@yandex.ru>
2023-05-31 19:00:15 +0300

    language changer in localStorage

--- js/language.js
+++ js/language.js
@@ -15,3 +15,32 @@ function modalHidden(e) {
 languageToolBarBtn.addEventListener('click', modalShow)
 
 document.addEventListener('click', modalHidden)
+
+// lang change
+
+const languageBtnENG = document.querySelector('.language-box-us');
+const languageBtnRUS = document.querySelector('.language-box-ru');
+
+function translateENG() {
+    localStorage.setItem('english', 'true');
+
+    document.getElementById('pageDocumentation').textContent = 'documentation'
+}
+
+function translateRUS() {
+    localStorage.setItem('english', 'false')
+
+    document.getElementById('pageDocumentation').textContent = 'документация'
+}
+
+languageBtnENG.addEventListener('click', translateENG);
+languageBtnRUS.addEventListener('click', translateRUS);
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('english') == 'true') {
+        translateENG()
+    }
+    else {
+        translateRUS()
+    }
+});
